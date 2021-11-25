07aba4a8523b9418f5396f12d7dab5bcfbab93af
ivanfsilva <ivanfs27@gmail.com>
2021-11-24 21:39:20 -0300

    adding phones

---
+++ src/main/java/escola/Telefone.java
@@ -0,0 +1,11 @@
+package escola;
+
+public class Telefone {
+    private String ddd;
+    private String numero;
+
+    public Telefone(String ddd, String numero) {
+        this.ddd = ddd;
+        this.numero = numero;
+    }
+}
