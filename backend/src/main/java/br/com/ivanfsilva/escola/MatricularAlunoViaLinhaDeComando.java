dc35655e4332886edeca1c9353c3e5d312e40572
ivanfsilva <ivanfs27@gmail.com>
2021-11-27 15:51:17 -0300

    indentation

--- backend/src/main/java/br/com/ivanfsilva/escola/MatricularAlunoViaLinhaDeComando.java
+++ backend/src/main/java/br/com/ivanfsilva/escola/MatricularAlunoViaLinhaDeComando.java
@@ -1,10 +1,14 @@
 package br.com.ivanfsilva.escola;
 
+import br.com.ivanfsilva.escola.shared.dominio.evento.PublicadorDeEventos;
+
 import br.com.ivanfsilva.escola.academico.aplicacao.aluno.matricular.MatricularAluno;
 import br.com.ivanfsilva.escola.academico.aplicacao.aluno.matricular.MatricularAlunoDTO;
-import br.com.ivanfsilva.escola.shared.dominio.evento.PublicadorDeEventos;
 import br.com.ivanfsilva.escola.academico.dominio.aluno.LogDeAlunoMatriculado;
 import br.com.ivanfsilva.escola.academico.infra.aluno.RepositorioDeAlunosEmMemoria;
+
+import br.com.ivanfsilva.escola.gamificacao.aplicacao.GeraSeloAlunoNovato;
+import br.com.ivanfsilva.escola.gamificacao.infra.selos.RepositorioDeSelosEmMemoria;
 
 public class MatricularAlunoViaLinhaDeComando {
 
@@ -15,8 +19,9 @@ public class MatricularAlunoViaLinhaDeComando {
 
         PublicadorDeEventos publicador = new PublicadorDeEventos();
         publicador.adicionar( new LogDeAlunoMatriculado() );
+        publicador.adicionar( new GeraSeloAlunoNovato( new RepositorioDeSelosEmMemoria() ) );
 
-        MatricularAluno matricular = new MatricularAluno( new RepositorioDeAlunosEmMemoria(), publicador);
+        MatricularAluno matricular = new MatricularAluno( new RepositorioDeAlunosEmMemoria(), publicador );
         matricular.executa( new MatricularAlunoDTO( nome, cpf, email ) );
     }
 }
